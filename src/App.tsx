470373db02462c52b7d12e891872d69a5c8ad698
Roan Yosky Timane <roanyosky@gmail.com>
2026-08-05 18:20:38 +0200

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>Service Request Portal</h1>
+      </header>
+      <main>
+        <p>Welcome to the Service Request Portal!</p>
+      </main>
+    </div>
+  )
+}
+
+export default App
